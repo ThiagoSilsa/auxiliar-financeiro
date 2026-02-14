27cdc8b45b2925a16988792a4df6aba77b84366d
ThiagoSilsa <thiagosantosprog@gmail.com>
2026-02-14 08:17:15 -0300

    feat(ExtratoPage): Estilizei colunas da tabela

--- src/app/(content)/extrato/config/columns.jsx
+++ src/app/(content)/extrato/config/columns.jsx
@@ -1,23 +1,54 @@
 import { Badge } from "@/components/ui/badge";
 import { FaArrowDown, FaArrowUp } from "react-icons/fa";
-
+import { formatCurrency, formatDate } from "@/lib/format";
 export const getExtratoColumns = () => [
   {
     accessorKey: "tipo",
     header: "Tipo",
-    enableSorting: false,
+    enableSorting: true,
     size: 50,
+    cell: ({ row }) =>
+      row.original.tipo === "entrada" ? (
+        <div className="flex items-center gap-1 text-ring/80 font-bold">
+          <FaArrowUp />
+          <p>Entrada</p>
+        </div>
+      ) : (
+        <div className="flex items-center gap-1 text-destructive/80 font-bold">
+          <FaArrowDown />
+          <p>Saída</p>
+        </div>
+      ),
+  },
+
+  { accessorKey: "descricao", header: "Descrição", size: 250 },
+  { accessorKey: "categoria", header: "Categoria", size: 75 },
+
+  {
+    accessorKey: "data",
+    header: "Data",
+    sortDescFirst: true,
+    size: 75,
+    cell: ({ row }) => formatDate(row.original.data),
+  },
+  { accessorKey: "conta", header: "Conta", size: 75 },
+  {
+    accessorKey: "valor",
+    header: "Valor",
+    size: 75,
     cell: ({ row }) => (
-      <Badge
-        variant={row.original.tipo === "entrada" ? "success" : "destructive"}
+      <span
+        className={` flex items-center font-bold
+          ${
+            row.original.tipo === "entrada"
+              ? "text-ring/80 "
+              : "text-destructive/80 "
+          }
+        `}
       >
-        {row.original.tipo === "entrada" ? <FaArrowDown /> : <FaArrowUp />}
-      </Badge>
+        {row.original.tipo === "entrada" ? <p className="w-2">+</p> : <p className="w-2 text-lg">-</p>}
+        {formatCurrency(row.original.valor)}
+      </span>
     ),
   },
-  { accessorKey: "descricao", header: "Descrição", size: 300 },
-  { accessorKey: "categoria", header: "Categoria", size: 75 },
-  { accessorKey: "valor", header: "Valor", size: 75 },
-  { accessorKey: "data", header: "Data", sortDescFirst: true, size: 75 },
-  { accessorKey: "conta", header: "Conta", size: 75 },
 ];
