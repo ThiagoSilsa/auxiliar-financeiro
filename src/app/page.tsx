580fc172eb3ceafd7a435ede14fe2ed8aee06bae
ThiagoSilsa <thiagosantosprog@gmail.com>
2026-02-18 13:57:20 -0300

    feat(LoginPage): Adicionei aria-label em botao de exibir senha, ajustei indentacao e corrigi erro de portugues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,6 @@ import {
   HoverCardContent,
   HoverCardTrigger,
 } from "@/components/ui/hover-card";
-import { Label } from "@/components/ui/label";
 import { Field, FieldGroup, FieldLabel, FieldSet } from "@/components/ui/field";
 
 const LoginPage = () => {
@@ -37,114 +36,115 @@ const LoginPage = () => {
         className="absolute top-4 right-4 flex gap-2 z-10"
         variant="construction"
       >
-        Em contrução
+        Em construção
       </Badge>
 
       <div className="flex min-h-screen flex-col items-center justify-center p-4 sm:p-8 md:p-12 lg:p-24">
-        <Card className="w-full max-w-md sm:max-w-lg p-4 -mt-30">
-            <CardHeader>
-              <CardTitle className="text-center font-black text-2xl sm:text-3xl text-chart-5">
-                SolarCash
-              </CardTitle>
-              <CardDescription></CardDescription>
-              <CardAction></CardAction>
-            </CardHeader>
-            <CardContent>
-              <FieldSet>
-                <FieldGroup>
-                  <Field>
-                    <FieldLabel htmlFor="email">E-mail</FieldLabel>
-                    <Input
-                      id="email"
-                      name="email"
-                      type="email"
-                      placeholder="Seu melhor e-mail"
-                      autoComplete="email"
-                    />
-                  </Field>
-                  <Field>
-                    <div className="flex justify-between items-center">
-                      <FieldLabel htmlFor="password">Senha</FieldLabel>
-                      <Button
-                        className="cursor-pointer"
-                        variant={"ghost"}
-                        size={"sm"}
-                        onClick={() => setCanSee(!canSee)}
-                      >
-                        {canSee ? <FaEye /> : <FaEyeSlash />}
-                      </Button>
-                    </div>
-                    <Input
-                      id="password"
-                      name="password"
-                      type={canSee ? "text" : "password"}
-                      placeholder="Sua senha secreta"
-                      autoComplete="new-password"
-                    />
-                  </Field>
-                </FieldGroup>
-              </FieldSet>
+        <Card className="w-full max-w-md sm:max-w-lg p-4 -mt-24">
+          <CardHeader>
+            <CardTitle className="text-center font-black text-2xl sm:text-3xl text-chart-5">
+              SolarCash
+            </CardTitle>
+            <CardDescription></CardDescription>
+            <CardAction></CardAction>
+          </CardHeader>
+          <CardContent>
+            <FieldSet>
+              <FieldGroup>
+                <Field>
+                  <FieldLabel htmlFor="email">E-mail</FieldLabel>
+                  <Input
+                    id="email"
+                    name="email"
+                    type="email"
+                    placeholder="Seu melhor e-mail"
+                    autoComplete="email"
+                  />
+                </Field>
+                <Field>
+                  <div className="flex justify-between items-center">
+                    <FieldLabel htmlFor="password">Senha</FieldLabel>
+                    <Button
+                      className="cursor-pointer"
+                      variant={"ghost"}
+                      size={"sm"}
+                      onClick={() => setCanSee(!canSee)}
+                      aria-label={canSee ? "Esconder senha" : "Mostrar senha"}
+                    >
+                      {canSee ? <FaEye /> : <FaEyeSlash />}
+                    </Button>
+                  </div>
+                  <Input
+                    id="password"
+                    name="password"
+                    type={canSee ? "text" : "password"}
+                    placeholder="Sua senha secreta"
+                    autoComplete="new-password"
+                  />
+                </Field>
+              </FieldGroup>
+            </FieldSet>
 
-              <div className="flex gap-2 items-center justify-center">
-                <Link href="/home">
-                  <Button
-                    onClick={() => handleAlert()}
-                    className="mt-4 w-full sm:w-auto cursor-pointer"
-                  >
-                    Entrar
-                  </Button>
-                </Link>
-              </div>
-            </CardContent>
-          </Card>
+            <div className="flex gap-2 items-center justify-center">
+              <Link href="/home">
+                <Button
+                  onClick={() => handleAlert()}
+                  className="mt-4 w-full sm:w-auto cursor-pointer"
+                >
+                  Entrar
+                </Button>
+              </Link>
+            </div>
+          </CardContent>
+        </Card>
 
         <div className="flex gap-3 sm:gap-5 absolute bottom-4 left-1/2 -translate-x-1/2">
           <a
             className="hover:scale-110 transition-transform"
-            href="https://github.com/thiagosilsa "
+            href="https://github.com/thiagosilsa"
             target="_blank"
             rel="noopener noreferrer"
           >
             <HoverCard>
               <HoverCardTrigger className="text-muted-foreground underline cursor-pointer text-2xl sm:text-3xl hover:text-primary">
                 <FaGithub />
-                </HoverCardTrigger>
-                <HoverCardContent>
-                  Visite meu GitHub{" "}
-                  <Link
-                    className="underline hover:text-primary"
-                    href="https://github.com/thiagosilsa "
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    @ThiagoSilsa
-                  </Link>
-                </HoverCardContent>
-              </HoverCard>
-            </a>
-            <Link
-              className="hover:scale-110 transition-transform"
-              href="https://www.linkedin.com/in/thiago-silsa/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <HoverCard>
-                <HoverCardTrigger className="text-muted-foreground underline cursor-pointer text-2xl sm:text-3xl hover:text-chart-2">
-                  <FaLinkedin />
-                </HoverCardTrigger>
-                <HoverCardContent>
-                  Visite meu LinkedIn{" "}
-                  <a
-                    className="underline hover:text-chart-2"
-                    href="https://www.linkedin.com/in/thiago-silsa/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    @ThiagoSilsa
-                  </a>
-                </HoverCardContent>
-              </HoverCard>
-            </Link>
+              </HoverCardTrigger>
+              <HoverCardContent>
+                Visite meu GitHub{" "}
+                <Link
+                  className="underline hover:text-primary"
+                  href="https://github.com/thiagosilsa "
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  @ThiagoSilsa
+                </Link>
+              </HoverCardContent>
+            </HoverCard>
+          </a>
+          <Link
+            className="hover:scale-110 transition-transform"
+            href="https://www.linkedin.com/in/thiago-silsa/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <HoverCard>
+              <HoverCardTrigger className="text-muted-foreground underline cursor-pointer text-2xl sm:text-3xl hover:text-chart-2">
+                <FaLinkedin />
+              </HoverCardTrigger>
+              <HoverCardContent>
+                Visite meu LinkedIn{" "}
+                <a
+                  className="underline hover:text-chart-2"
+                  href="https://www.linkedin.com/in/thiago-silsa/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  @ThiagoSilsa
+                </a>
+              </HoverCardContent>
+            </HoverCard>
+          </Link>
         </div>
       </div>
     </main>
