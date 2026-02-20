ab0778929b538952eed9597d3092474d22b6e43f
ThiagoSilsa <thiagosantosprog@gmail.com>
2026-02-20 07:04:41 -0300

    feat(Header): Adicionei icones e estado que deixa claro qual a página atual

--- src/components/created/header/header.tsx
+++ src/components/created/header/header.tsx
@@ -1,9 +1,19 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import Link from "next/link";
-import { MenuIcon, MoonIcon, SunIcon } from "lucide-react";
+// React
+import { useState } from "react";
 
+// Next
+import Link from "next/link";
+
+// icons
+import { MenuIcon, MoonIcon, SunIcon } from "lucide-react";
+import { IoHomeOutline } from "react-icons/io5";
+import { RiBillLine } from "react-icons/ri";
+import { CiBank } from "react-icons/ci";
+import { LuPiggyBank } from "react-icons/lu";
+
+// Components
 import { Button } from "@/components/ui/button";
 import {
   Sheet,
@@ -12,17 +22,29 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 
+// Theme
 import { useTheme } from "next-themes";
 
+// Útil
+import { cn } from "@/lib/utils";
+
+// Links de navegação
 const NAV_LINKS = [
-  { href: "/home", label: "Home" },
-  { href: "/extrato", label: "Extrato" },
-  { href: "/contas", label: "Contas" },
-  { href: "/caixinhas", label: "Caixinhas" },
+  { href: "/home", label: "Home", icon: IoHomeOutline },
+  { href: "/extrato", label: "Extrato", icon: RiBillLine },
+  { href: "/contas", label: "Contas", icon: CiBank },
+  { href: "/caixinhas", label: "Caixinhas", icon: LuPiggyBank },
 ];
 
 const Header = () => {
   const { theme, setTheme } = useTheme();
+  const [actualPage, setActualPage] = useState("/home");
+  const [isSheetOpen, setIsSheetOpen] = useState(false);
+
+  const handlePageChange = (href: string) => {
+    setActualPage(href);
+    setIsSheetOpen(false);
+  };
 
   return (
     <header className="sticky top-0 z-40 w-full shadow backdrop-blur supports-backdrop-filter:bg-card/60">
@@ -40,7 +62,15 @@ const Header = () => {
         <nav className="hidden items-center gap-6 sm:flex">
           {NAV_LINKS.map((link) => (
             <Link key={link.href} href={link.href}>
-              <Button variant="ghost" size="sm">
+              <Button
+                variant="ghost"
+                size="sm"
+                className={
+                  actualPage === link.href ? "bg-primary text-white" : ""
+                }
+                onClick={() => setActualPage(link.href)}
+              >
+                {link.icon && <link.icon className="size-4 mr-2" />}
                 {link.label}
               </Button>
             </Link>
@@ -61,7 +91,7 @@ const Header = () => {
             )}
           </Button>
         </div>
-        <Sheet>
+        <Sheet open={isSheetOpen} onOpenChange={(open) => setIsSheetOpen(open)}>
           <SheetTrigger className="sm:hidden">
             <MenuIcon className="p-0.5 rounded cursor-pointer hover:bg-primary/80 hover:text-white dark:hover:bg-primary/50 text-chart-5 " />
           </SheetTrigger>
@@ -77,7 +107,16 @@ const Header = () => {
             <div className="flex flex-col mt-4 w-full items-center">
               {NAV_LINKS.map((link) => (
                 <Link key={link.href} href={link.href}>
-                  <Button variant="ghost" size="sm" className="w-xs mb-2">
+                  <Button
+                    variant="ghost"
+                    size="sm"
+                    className={cn(
+                      "w-full justify-start mb-2",
+                      actualPage === link.href ? "bg-primary text-white" : "",
+                    )}
+                    onClick={() => handlePageChange(link.href)}
+                  >
+                    {link.icon && <link.icon className="size-4 mr-2" />}
                     {link.label}
                   </Button>
                 </Link>
